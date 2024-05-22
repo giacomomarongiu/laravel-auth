d35ad81a42de813ebf95a5e0bc1375c18b6d8b80
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-05-22 12:07:30 +0200

    [Working] Created a table with migration + Created a Seeder with Faker

--- database/migrations/2024_05_21_125619_create_projects_table.php
+++ database/migrations/2024_05_21_125619_create_projects_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration {
     /**
      * Run the migrations.
@@ -12,8 +13,8 @@ return new class extends Migration {
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 50)->unique();
-            $table->string('slug', 50);
+            $table->string('title', 250)->unique();
+            $table->string('slug', 250);
             $table->string('img')->nullable();
             $table->date('start_date')->nullable();
             $table->date('end_date')->nullable();
